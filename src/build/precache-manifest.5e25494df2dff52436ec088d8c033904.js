1cc001c3ba6f0b90dfcc9d674a8e625ef2882c77
Alfredo Alea <alfredoalea@gmail.com>
2020-06-04 17:23:35 -0400

    First Commit

---
+++ src/build/precache-manifest.5e25494df2dff52436ec088d8c033904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad1a692bea32a70b9759d6367002a936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36d9d596d9905ebeaeca",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "490549ede20bbe789b42",
+    "url": "/static/js/2.eef04ac1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eef04ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d9d596d9905ebeaeca",
+    "url": "/static/js/main.3280feac.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
